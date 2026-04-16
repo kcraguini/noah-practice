5f20e550436d534b24505c26781faa9ec6bf7622
gxk27 <100238310+gxk27@users.noreply.github.com>
2026-04-15 22:09:24 -0500

    passwordGenerator final Changes

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,5 +1,5 @@
 import random
-
+#lists of the letters, numbers, and desired symbols to set up the password
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
              "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
@@ -9,12 +9,14 @@ symbols = ["~","!","@","#","$","%","*","(",")","_","-","+","=","|","/","?",";","
 
 print("I am here to generate a password for you!")
 
+#user gets to input how long they want their password to be and how many letters, symbols, or numbers the user wants
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like in your password?\n"))
 nr_numbers = int(input("How many numbers would you like in your password?\n"))
 
 password = []
 
+#grabbing the range of every list randomly 
 for char in range(0, nr_letters):
     password.append(random.choice(alphabet))
 
@@ -24,11 +26,11 @@ for char in range(0, nr_symbols):
 for char in range(0, nr_numbers):
     password.append(random.choice(numbers))
 
-print(password)
+#shuffles the password so it is more difficult to read
 random.shuffle(password)
 
-password_list = ""
-for char in password_list():
-    password_list += char
+password_string = ""
+for char in password: 
+    password_string += char
 
-print(f"Your random Password: {password_list}\n No Hacker will ever guess it :D")
+print(f"Your random Password: {password_string}\nNo Hacker will ever guess it :D")
