1ce4286a12a9ee1c11d332f80e7dbcfad0b90ea4
gxk27 <100238310+gxk27@users.noreply.github.com>
2026-04-22 21:28:18 -0500

    Add files via upload

--- chatbotAI.py
+++ chatbotAI.py
@@ -1,22 +1,36 @@
-def response(bot):
-    if bot.lower() == "hello":
+#importing Natural Language ToolKit
+import nltk
+#downloading 'punkt_tab' which is essentially a language file from the internet.
+nltk.download('punkt_tab')
+#grabbing one tool from the toolbox NLTK
+from nltk.tokenize import word_tokenize
+
+#creating a funciton that will tokenize what the user inputs "reading the input based off keywords"
+def tokensizer(bot):
+    tokens = word_tokenize(bot)
+    return tokens
+
+def response(tokens):
+    if "hello" in tokens:
         print("Hey There!")
 
-    elif bot.lower() == "how are you?":
+    elif "how are you?" in tokens:
         print("Im great thanks!")
 
-    elif bot.lower() == "help":
+    elif "help" in tokens:
         print("I can help you with whatever you need!")
 
     else:
         print("I do not undertsand")
     
 while True:
+
     #Chatbot asking you what you need
     bot = input("What can I help you with?")
-    
+
     if bot.lower() == "quit":
         print("Goodbye!")
         break
-
-    response(bot)
+    #calling the tokenizor function and setting the parameter to "bot", so it reads your input
+    tokens = tokensizer(bot)
+    response(tokens)
